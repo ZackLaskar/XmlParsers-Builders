3c592f3f3339c8f4ab86ed6293ca6185e96a283d
zackLaskar <mzak.hussain@gmail.com>
2018-10-26 21:57:11 +0200

    Xml Parser learning.

---
+++ src/xml/Parser/main/xmlParseBuild.groovy
@@ -0,0 +1,30 @@
+package main
+
+//https://stackoverflow.com/questions/15077592/iterate-through-each-xml-node-with-groovy-printing-each-node
+/*
+
+xml attributes to map
+https://coderanch.com/t/620651/languages/convert-xml-tag-attribute-values
+
+generate XML from MAP
+https://coderanch.com/t/471377/languages/generate-XML-MAP
+https://stackoverflow.com/questions/48709822/dynamically-generate-xml-with-attributes-and-child-nodes-from-map-in-groovy
+ */
+doc = new XmlSlurper().parse("/Users/Muzakkir/IdeaProjects/XmlParsers-Builders/src/samp/files/test.xml")
+
+doc.deployables.each { thing ->
+    thing.children().each { tag ->
+        println "---------------------"
+        //println "${tag.name()}"
+        map1 = "${tag.name()}"
+        println map1
+        def map1 = [:]
+        println "---------------------"
+        tag.children().each { child ->
+            println "  ${child.name()}: ${child.text()}"
+            map1[ "${child.name()}" ] = [ "${child.text()}" ]
+        }
+        println(map1)
+    }
+}
+
